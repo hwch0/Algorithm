49a8b57077362c758f80cbf55a6ef18e6a6b4ed3
hwch0 <95171615+hwch0@users.noreply.github.com>
2023-04-27 01:38:57 +0900

    [level 2] Title: 의상, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -4,17 +4,13 @@ def solution(clothes) :
     
     clothes = [c[1] for c in clothes]
     counter = dict(Counter(clothes))
-    
-    s = 0
-    
 
     if len(counter) == 1:
-        
         return len(clothes)
 
     else: 
         i = 1
         for clothe in counter.values():
             i *=(clothe+1)
-           
+            
     return i-1
